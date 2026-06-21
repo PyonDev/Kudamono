be5917d33d162606ae2ba84fc9b1d39df61eb3bb
NyanPanDev <nekonyansoft@proton.me>
2026-06-21 19:46:54 +0100

    Initialized Service

---
+++ kudamono-service/src/main/java/com/nyanpan/kudamono/KudamonoApplication.java
@@ -0,0 +1,13 @@
+package com.nyanpan.kudamono;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KudamonoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KudamonoApplication.class, args);
+	}
+
+}
---
+++ kudamono-service/src/test/java/com/nyanpan/kudamono/KudamonoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nyanpan.kudamono;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KudamonoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
